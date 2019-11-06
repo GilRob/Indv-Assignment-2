995cfa1dd25b0727eb3fc3f336b442628fa05a42
Gil <gil.robern@ontariotechu.net>
2019-11-06 16:30:45 -0500

    Added Name and Student ID
    Added my name and student number to the scripts I edited, which include:
    BulletController
    BulletPoolmanager
    PlayerController

--- Mail Pilot/Mail Pilot/Assets/_Scripts/BulletPoolManager.cs
+++ Mail Pilot/Mail Pilot/Assets/_Scripts/BulletPoolManager.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿//Gil Robern - 100651824
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
--- Mail Pilot/Mail Pilot/Assets/_Scripts/PlayerController.cs
+++ Mail Pilot/Mail Pilot/Assets/_Scripts/PlayerController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿//Gil Robern - 100651824
+
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
